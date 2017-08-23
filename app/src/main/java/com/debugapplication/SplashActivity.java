620ffd626431259c9bf0b42b94de978f52cd78b8
DESKTOP-VJC6IHR\King6rf <3521829873@qq.com>
2017-08-23 17:08:55 +0800

    1231

--- app/src/main/java/com/debugapplication/SplashActivity.java
+++ app/src/main/java/com/debugapplication/SplashActivity.java
@@ -1,16 +1,8 @@
 package com.debugapplication;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 /**
  * Created by Administrator on 2017/7/16.
@@ -21,39 +13,18 @@ public class SplashActivity extends Activity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        activity=this;
-        FrameLayout parent=new FrameLayout(activity);
-        parent.setBackgroundColor(Color.parseColor("#00000000"));
-        LinearLayout backlinear=new LinearLayout(activity);
-        backlinear.setId(0x7f07000a);
-        backlinear.setOrientation(LinearLayout.VERTICAL);
-        backlinear.setBackground(ImageUtilz.loadImageFromAsserts(activity, "d3mbackh.jpg"));
-        backlinear.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-        TextView textView1=new TextView(activity);
-        textView1.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,0,1));
-        TextView textView2=new TextView(activity);
-        textView2.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,0,1));
-        ImageView d3mlogo=new ImageView(activity);
-        d3mlogo.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,0,1));
-        d3mlogo.setScaleType(ImageView.ScaleType.FIT_CENTER);
-        d3mlogo.setImageDrawable(ImageUtilz.loadImageFromAsserts(activity,"d3mlogo.png"));
-        backlinear.addView(textView1);
-        backlinear.addView(d3mlogo);
-        backlinear.addView(textView2);
-        parent.addView(backlinear);
-        ViewGroup viewGroup= (ViewGroup) activity.findViewById(android.R.id.content);
-        viewGroup.addView(parent);
-        setContentView(parent);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    startActivity(new Intent(SplashActivity.this,Class.forName("com.unity3d.player.UnityPlayerActivity")));
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                }
-                finish();
-            }
-        },Integer.parseInt("1000"));
+        InjectActivity.getInstance().setActivity(this).initView();
+        DebugUtil.sendDelayFloatMessage();
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    startActivity(new Intent(SplashActivity.this,Class.forName("com.unity3d.player.UnityPlayerActivity")));
+//                } catch (ClassNotFoundException e) {
+//                    e.printStackTrace();
+//                }
+//                finish();
+//            }
+//        },Integer.parseInt("1000"));
     }
 }
